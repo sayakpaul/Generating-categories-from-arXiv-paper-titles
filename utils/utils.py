18eb87014d3f0ee44649d91549018d4c0bad22fd
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-10-31 11:47:13 +0000

    Added minor tweaks

--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,16 @@
+from sklearn.preprocessing import MultiLabelBinarizer
 import numpy as np
+import pickle
 import nltk
+import re
+
+nltk.download('stopwords')
 
 wpt = nltk.WordPunctTokenizer()
 stop_words = nltk.corpus.stopwords.words('english')
 
 # Function courtesy: http://bit.ly/2N17Clt
-def clean_title(title: ):
+def clean_title(title: np.ndarray) -> np.ndarray:
     # lower case and remove special characters\whitespaces
     title = re.sub(r'[^a-zA-Z\s]', '', title, re.I|re.A)
     title = title.lower()
@@ -17,3 +22,4 @@ def clean_title(title: ):
     # re-create document from filtered tokens
     title = ' '.join(filtered_tokens)
     return title
+    
